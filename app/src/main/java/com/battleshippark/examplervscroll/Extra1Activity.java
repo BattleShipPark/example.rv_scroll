789266192557ac6e4173693bf77ff82430b7f326
BattleShip Park <battleship.park@gmail.com>
2016-09-19 17:30:30 +0900

    - add butterknife - add Extra1Activity, Extra2Activity to show reverseLayout, stackFromEnd - add smoothScrollToPosition()

--- app/src/main/java/com/battleshippark/examplervscroll/Extra1Activity.java
+++ app/src/main/java/com/battleshippark/examplervscroll/Extra1Activity.java
@@ -1,44 +1,42 @@
 package com.battleshippark.examplervscroll;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.recyclerView)
-    RecyclerView recyclerView;
+public class Extra1Activity extends AppCompatActivity {
+    @BindViews({R.id.recyclerView1, R.id.recyclerView2})
+    List<RecyclerView> recyclerViews;
 
     private MainAdapter adapter;
-    private LinearLayoutManager layoutManager;
+    private List<LinearLayoutManager> layoutManagers = new ArrayList<>();
     private List<Integer> items;
     private int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_extra1);
         ButterKnife.bind(this);
 
-        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(layoutManager);
+        layoutManagers.add(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        layoutManagers.add(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
 
         items = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
@@ -46,62 +44,33 @@ public class MainActivity extends AppCompatActivity {
         }
         adapter = new MainAdapter(this);
         adapter.setItems(items);
-        recyclerView.setAdapter(adapter);
+
+        for (int i = 0; i < 2; i++) {
+            recyclerViews.get(i).setLayoutManager(layoutManagers.get(i));
+            recyclerViews.get(i).setAdapter(adapter);
+        }
+    }
+
+    @OnClick(R.id.checkbox1)
+    void onClickCheck1(CheckBox checkBox) {
+        for (int i = 0; i < 2; i++) {
+            layoutManagers.get(i).setReverseLayout(checkBox.isChecked());
+        }
     }
 
     @OnClick(R.id.btn1)
     void onClickBtn1() {
         position = (position + 6) % 20;
-        recyclerView.scrollToPosition(position);
+        for (int i = 0; i < 2; i++) {
+            layoutManagers.get(i).scrollToPosition(position);
+        }
     }
 
     @OnClick(R.id.btn2)
     void onClickBtn2() {
         position = (position + 6) % 20;
-        layoutManager.scrollToPositionWithOffset(position, 20);
-    }
-
-    @OnClick(R.id.btn3)
-    void onClickBtn3() {
-        startActivity(new Intent(this, Extra1Activity.class));
-    }
-
-    @OnClick(R.id.btn4)
-    void onClickBtn4() {
-        startActivity(new Intent(this, Extra2Activity.class));
-    }
-
-    @OnClick(R.id.btn5)
-    void onClickBtn5() {
-        position = (position + 6) % 20;
-        RecyclerView.SmoothScroller smoothScroller = new CustomSmoothScroller(recyclerView.getContext(), layoutManager, 30);
-        smoothScroller.setTargetPosition(position);
-        layoutManager.startSmoothScroll(smoothScroller);
-    }
-
-    private static class CustomSmoothScroller extends LinearSmoothScroller {
-        private LinearLayoutManager layoutManager;
-        private int centerOffset;
-
-        public CustomSmoothScroller(Context context, LinearLayoutManager layoutManager, int centerOffset) {
-            super(context);
-            this.layoutManager = layoutManager;
-            this.centerOffset = centerOffset;
-        }
-
-        @Override
-        public PointF computeScrollVectorForPosition(int targetPosition) {
-            return layoutManager.computeScrollVectorForPosition(targetPosition);
-        }
-
-        @Override
-        protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
-            return super.calculateSpeedPerPixel(displayMetrics) * 3;
-        }
-
-        @Override
-        public int calculateDxToMakeVisible(View view, int snapPreference) {
-            return super.calculateDxToMakeVisible(view, SNAP_TO_START) + centerOffset;
+        for (int i = 0; i < 2; i++) {
+            layoutManagers.get(i).scrollToPositionWithOffset(position, 20);
         }
     }
 
@@ -138,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(MainViewHolder holder, int position) {
-            holder.bind(position);
+            holder.bind(items.get(position));
         }
 
         @Override
